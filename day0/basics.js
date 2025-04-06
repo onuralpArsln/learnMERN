5fc5464100340bfbb3967e49ad9d7faf48266e2f
Onuralp <58907470+onuralpArsln@users.noreply.github.com>
2025-04-06 13:19:03 +0000

    first lines

---
+++ day0/basics.js
@@ -0,0 +1,11 @@
+let name = "John";  // String mutable
+const age = 25;     // Number immutable
+let isStudent = true;  // Boolean
+
+
+function greet() {
+    console.log("Hello, world!");
+  }
+  
+  greet(); // Calling the function
+  
