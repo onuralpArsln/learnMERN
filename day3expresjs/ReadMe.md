93ad13fde5a0cf8050570e6714fbfa408101f982
onuralpArlsn <onuralparslan97@gmail.com>
2025-04-25 18:42:15 +0300

    added files back

---
+++ day3expresjs/ReadMe.md
@@ -0,0 +1,19 @@
+# Başlangıç 
+
+Yapılan yüklemeleri takip etmek için 
+> npm init -y
+Böylece `npm start` ile çalıştırma `npm install` ile package.jsonda hızlı kurulmuna olanak verir
+
+# Yüklemeler
+
+express js için 
+>npm install express
+
+live refresh ile ( kod  güncellemeleri direkt servera yansır)
+>npm install --save-dev nodemon
+
+# Çalıştırmak
+
+>node server.js
+or
+>npm run dev
---
+++ day3expresjs/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//  Basit bir API endpoint
+app.get('/api/selam', (req, res) => {
+    res.json({ mesaj: 'Selam' });
+});
+
+// URL parametre alma başında : ile parametre oldugunu belirt
+app.get('/api/selam/:isim', (req, res) => {
+    const isim = req.params.isim;
+    res.json({ mesaj: `Selam, ${isim}!` }); // ${isim} ile değişkenden çek 
+});
+// http://localhost:3000/api/selam/Alp
+
+
+//Query  youtube tarzı 
+app.get('/api/selam2', (req, res) => {
+    const isim = req.query.isim;
+    res.json({ mesaj: `Selam, ${isim}!` });
+});
+// http://localhost:3000/api/selam?isim=Alp
+
+// sayı alıp işlem yapma 
+
+app.get('/api/topla', (req, res) => {
+    // Query parametrelerinden 'sayi1' ve 'sayi2' alınır
+    const sayi1 = parseInt(req.query.sayi1); // gelen stringi int yaptık
+    const sayi2 = parseInt(req.query.sayi2);
+
+    // İki sayıyı topla
+    const toplam = sayi1 + sayi2;
+
+    // Sonucu JSON formatında döndür
+    res.json({ toplam: toplam });
+});
+
+// http://localhost:3000/api/topla?sayi1=5&sayi2=7
+
+// HTML sayfası döndürme
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// HTML sayfası döndürme
+app.get('/second', (req, res) => {
+    res.sendFile(__dirname + '/second.html');
+});
+
+// Sunucuyu başlat
+app.listen(PORT, () => {
+    console.log(`Sunucu çalışıyor http://localhost:${PORT}`);
+});
